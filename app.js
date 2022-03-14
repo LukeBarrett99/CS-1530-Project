0edda47f273cd0674916567ad550766d664ef15e
eanderson <717eanderson@gmail.com>
2022-03-14 16:43:29 -0400

    basic express setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('stuff'));
+app.listen(3000);
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/create-post', (req, res) => {
+  res.render('create-post');
+});
+
+app.post('/create-post', urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.render('post-created-successfully', {data: req.body});
+})
